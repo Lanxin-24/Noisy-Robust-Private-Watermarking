18dccd32192b15868ffdb1797cd78a8b4957fd52
Lanxin Luo <165103754+Lanxin-24@users.noreply.github.com>
2024-03-31 19:38:58 +0800

    Update Numerical_Example.py

--- NoisyRPW/Numerical_Example.py
+++ NoisyRPW/Numerical_Example.py
@@ -6,21 +6,23 @@ import numpy as np
 from scipy.stats import norm
 
 
+
 # Define parameters
 prompt = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']
-key = b'\xd3-\xf9\xf5\xbd\xf1\xdc\x0e\x1e.\x9a\xc2f\x17\x01\x18l6\x81\x1a\xdc4\xa2\xe1\x0f\x8b\xf8\xdd\x86\xd3P'
+key = secrets.token_bytes(32)
 h = 3
 beta = 0.5
-delta = 5
-n_values = range(1, 201)  
-sigmas = [0, 0.25, 0.5, 0.75, 1.0]
+delta = 10
+n_max = 200
+n_values = range(1, 201) 
+sigmas = [0.25, 0.5, 0.75, 1.0]
 
 
+
+fig, axes = plt.subplots(1, 2, figsize=(15, 6))
 
 # Generate and plot Z-scores and n for different sigma values
-plt.clf()
-plt.figure(constrained_layout=True)
-plt.figure(figsize=(10, 6))
+ax1 = axes[0]
 for sigma in sigmas:
     z_values = []
     for n in n_values:
@@ -28,43 +30,42 @@ for sigma in sigmas:
         green_list_count = Detection.count_green_list_words(prompt, prompt_result, key, beta, h)
         z_scores = Detection.compute_z_score(green_list_count, beta, n)
         z_values.append(z_scores)
-    plt.plot(n_values, z_values, label=f"sigma = {sigma}")
+    ax1.plot(n_values, z_values, label=f"sigma = {sigma}")
 
-plt.title("Z-Score vs. n for Different Sigma Values")
-plt.xlabel("n")
-plt.ylabel("Z-Score")
-plt.legend()
-plt.grid(True)
-plt.show()
+ax1.set_title("Z-Score vs. n for Different Sigma Values")
+ax1.set_xlabel("n")
+ax1.set_ylabel("Z-Score")
+ax1.legend()
+ax1.grid(True)
 
 
 
 # Plot trade-off funcyion for different n and sigma values
 def calculate_mu(n, h, sigma):
-    mu = np.sqrt(2 * n) / h * np.sqrt(np.exp(sigma ** (-2)) * norm.cdf(1.5 * sigma ** -1) + 3 * norm.cdf(-0.5 * sigma ** -1) - 2)
+    mu = np.sqrt(2 * n) / h * np.sqrt(np.exp(sigma ** (-2)) * norm.cdf(1.5 * sigma ** (-1)) + 3 * norm.cdf(-0.5 * sigma ** (-1)) - 2)
     return mu
 
 def G_mu(mu, alpha):
     return norm.cdf(norm.ppf(1 - alpha) - mu)
 
-n_values_sparse = [10, 50, 100, 200]
+n_values_sparse = [10, 100, 200]
+sigmas_sparse = [0.5, 1.0]
 alpha_values = np.linspace(0.001, 0.999, 100)
 
-plt.clf()
-plt.figure(constrained_layout=True)
-plt.figure(figsize=(10, 6))
-
+ax2 = axes[1]
 for n in n_values:
     for sigma in sigma_values:
         mu = calculate_mu(n, h, sigma)
         G_mu_values = G_mu(mu, alpha_values)
-        plt.plot(alpha_values, G_mu_values, label=f'n={n}, sigma={sigma}')
+        ax2.plot(alpha_values, G_mu_values, label=f'n={n}, sigma={sigma}')
 
-plt.title("GDP for Different n and Sigma Values")
-plt.xlabel("Type I Error")
-plt.ylabel("Type II Error")
-plt.legend()
-plt.grid(True)
+ax2.set_title("GDP for Different n and Sigma Values")
+ax2.set_xlabel("Type I Error")
+ax2.set_ylabel("Type II Error")
+ax2.legend()
+ax2.grid(True)
+
+plt.tight_layout()
 plt.show()
 
 
