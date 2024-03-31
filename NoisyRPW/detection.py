e6f508975a51fe2ecfaca51e5f7bc76ca59661b3
Lanxin Luo <165103754+Lanxin-24@users.noreply.github.com>
2024-03-31 08:47:58 +0800

    Update detection.py

--- NoisyRPW/detection.py
+++ NoisyRPW/detection.py
@@ -1,26 +1,25 @@
-def is_green_list_word(word, key, beta):
-    # Compute H and i* for the given word
-    H_star = float('inf')
-    for i in range(1, h+1):
+def count_green_list_words(prompt, prompt_result, key, beta, h):
+    green_list_count = 0
+    N_p = len(prompt)
+
+    # Check if the word t is a green list word and count
+    for t in range(N_p, len(prompt_result)):    
+        word = prompt_result[t]
+        
+        # Compute H and i* for the given word
+        H_star = float('inf')
+        for i in range(1, h+1):
         H = hmac.new(key, digestmod=hashlib.sha3_256)
         H.update(word.encode())
-        H.update(prompt[t - i].encode())
+        H.update(prompt_result[t - i].encode())
         H_i = int.from_bytes(H.digest(), byteorder='big')
         if H_i < H_star:
             H_star = H_i
 
-    # Generate a random bit based on H_i*
-    random.seed(H_star)
-    rand = random.random()
-    return rand < beta
-
-
-def count_green_list_words(prompt_result):
-    green_list_count = 0
-    
-    for word in prompt_result:
-        # Check if the word is a green list word
-        if is_green_list_word(word):
+        # Generate a random bit based on H_i*
+        random.seed(H_star)
+        rand = random.random()
+        if rand < beta
             green_list_count += 1
             
     return green_list_count
